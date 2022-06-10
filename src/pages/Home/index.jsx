f1e42ece34443f2e00fa070aeb398f0ea2c0161c
CharlesVeneau <veneauc@hotmail.fr>
2022-06-10 22:05:56 +0200

    Add Hero to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,20 @@
+//import Features from '../components/Features';
+
 function Home() {
-  return ( <div className="Home"><h1>This is Home</h1></div> );
+  return (
+    <main>
+      <section className="hero">
+        <article className="hero-content">
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
+        </article>
+      </section>
+      {/* <Features /> */}
+    </main>
+  );
 }
 
 export default Home;
