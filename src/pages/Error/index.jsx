40428ec335d95e81d32a88c607aa17d320c45098
CharlesVeneau <veneauc@hotmail.fr>
2022-06-17 11:26:14 +0200

    Add error 404 page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,19 @@
+import ErrorImage from '../../assets/images/404-Error-amico.png';
+import { NavLink } from 'react-router-dom';
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <section className="error">
+      <img src={ErrorImage} alt="erreur 404" className="error-image" />
+      <h1 className="error-title">Erreur 404</h1>
+      <p className="error-text">
+        La page que vous demandez n'existe pas, ou n'a pas été trouvée.
+      </p>
+      <NavLink to="/" className="error-link">
+        Retourner à l'accueil
+      </NavLink>
+    </section>
+  );
 }
 
 export default Error;
