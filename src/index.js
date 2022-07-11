43c59022a28d4a5f3ea7ff33734b920300a93db7
CharlesVeneau <veneauc@hotmail.fr>
2022-07-11 09:47:27 +0200

    Add routes to index

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from './store/store';
+import { Provider } from 'react-redux';
 import './Sass/index.scss';
 import App from './pages/App';
 import Home from './pages/Home';
-import Signin from './pages/Signin';
-import User from './pages/User';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import Error from './pages/Error';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import PrivateRoute from './PrivateRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="signin" element={<Signin />} />
-          <Route path="user/:userId" element={<User />} />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <PrivateRoute exact path="dashboard"></PrivateRoute>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
