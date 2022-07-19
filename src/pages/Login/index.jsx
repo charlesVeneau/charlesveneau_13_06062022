77e7044b66e97356fa75c8c1dbd94860da9c5a7f
CharlesVeneau <veneauc@hotmail.fr>
2022-07-19 18:44:40 +0200

    remove a false positive error on login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -35,7 +35,7 @@ function Login() {
       };
       dispatch(login(credentials));
       console.log(status);
-      if (status === 400 || !status) {
+      if (status === 400) {
         emailInput.classList.add('has-error');
         passwordInput.classList.add('has-error');
         errorMsg.classList.remove('isHidden');
